0aaee3acd7b51d119ded0a82410893b27918d216
UJeongff <jeomi364@gmail.com>
2026-05-01 10:20:43 +0900

    [swea] 2071_평균값구하기

--- SWEA/2071.py
+++ SWEA/2071.py
@@ -2,3 +2,15 @@
 # 10개의 수를 입력 받아, 평균값을 출력하는 프로그램을 작성하라. 
 
 # 출처: https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AV5QRnJqA5cDFAUq&categoryId=AV5QRnJqA5cDFAUq&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=PYTHON&select-1=1&pageSize=10&pageIndex=1
+
+T = int(input())
+for test_case in range(1, T+1):
+    numbers = map(int, input().split())
+    total = 0
+    average = 0
+
+    for num in numbers:
+        total += num
+        
+    average = total / 10
+    print(f"#{test_case} {round(average)}")
