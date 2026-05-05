28943345c6d04f1cc25865ebf01fb25d2dbc01ab
UJeongff <jeomi364@gmail.com>
2026-05-05 21:07:18 +0900

    [SWEA] 1859_백만장자

---
+++ SWEA/1859.py
@@ -0,0 +1,6 @@
+# 백만 장자 프로젝트
+# 사재기를 하여 최대한 이득을 얻도록 도와주자. 
+
+# 문제: https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=2&contestProbId=AV5LrsUaDxcDFAXc&categoryId=AV5LrsUaDxcDFAXc&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=2&pageSize=10&pageIndex=1
+
+T = int(input())
